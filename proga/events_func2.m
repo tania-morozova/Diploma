e1677441b514ad69234b959cf6f722e02b68aa4c
Tmoroz0077 <tmoroz0077@gmail.com>
2018-03-19 09:26:47 +0300

    first steps

--- proga/events_func2.m
+++ proga/events_func2.m
@@ -1,8 +1,8 @@
-function [value,isterminal,direction] = events_func(t,x,P,u2)
+function [value,isterminal,direction] = events_func2(t,x,P,u2)
     P_min = P(u2(1));
     P_max = P(u2(2));
     
     value = [x(1) - P_min(1), (x(2) - P_min(2))*(x(2) - P_max(2))];
     isterminal = [1,0];
-    direction = [0,-1];
+    direction = [0,1];
 end
